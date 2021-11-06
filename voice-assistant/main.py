976c8aec4092f4df4133852bcf15b7493ea26292
nirbhaysinghnarang <nsn8@cornell.edu>
2021-11-05 21:14:11 -0400

    created IntentRecog Class

--- voice-assistant/main.py
+++ voice-assistant/main.py
@@ -1,25 +1,44 @@
 import requests
 import json
-headers = {
-    "Content-Type": "application/json; charset=utf-8",
-    "Authorization": "Bearer ya29.a0ARrdaM-TzuYLuEdDgDZ-hpW8XotgmtMn3wTYEsdawxUYMd4TLNHRyxaLzP6zAtZ5ffvNXdyVA6Nx_GcNcxgraJpSpGKqQTv6bUUBXJJl_LqO_pQKwXw5xgp7Z9dCMecSrRU-ZjgLFGtcmTE0zd3YMq_FMeZJ_RNBRev9fazHSXr4E6A_b5hPHvBYLJ8IW1QhzrG82ul5063LhI3s7Lw_SdgRyNC7cK1tolgGpPX3SyEIBw"
-}
-data = {
-    "queryInput":{
-            "text":{
-                "text":
-                    "where can i get a haircut?",
-                    "languageCode":"en"
-                    }
-        },"queryParams":{
-                "source":"DIALOGFLOW_CONSOLE",
-                "timeZone":"America/New_York",
-                "sentimentAnalysisRequestConfig":{
-                    "analyzeQueryTextSentiment":True
-                    }
-                }
-        }
 
-url = "https://dialogflow.googleapis.com/v2/projects/megaagent-borp/agent/sessions/92ab73d7-dd71-9347-f491-5b4260961214:detectIntent"
-x = requests.post(url, data = json.dumps(data), headers=headers)
-print(x.text)
+class IntentRecognizer:
+    def __init__(self, text):
+        self.text = text
+        self.headers = {
+            "Content-Type": "application/json; charset=utf-8",
+            "Authorization": "Bearer ya29.a0ARrdaM8EQ1uADEdULXgvII5S6AwEjzEAWTENYs0Go0pncmgkuSTizjm1bFKU_yYumSbxmh7SXnUrWtmAF9wCv_PXibRvv_5P986JsB9NJ_x9YCw6zNfIR3l4FyJLOvyddxRkx0WmTYf57k4Eb9bQ9u3C3i2lqNHNywsAXhlK7PVnC4ycqiQjXEsQ8Ild-yHGpqAOPs5sIOZTeKbl5PQaSbjJCyAzvyx3BJdYH_jqxiKWtq0"
+        }
+        self.data = {
+            "queryInput":{
+                    "text":{
+                        "text":
+                            f"{self.text}",
+                            "languageCode":"en"
+                            }
+                },"queryParams":{
+                        "source":"DIALOGFLOW_CONSOLE",
+                        "timeZone":"America/New_York",
+                        "sentimentAnalysisRequestConfig":{
+                            "analyzeQueryTextSentiment":True
+                            }
+                        }
+                }
+        self.url = "https://dialogflow.googleapis.com/v2/projects/megaagent-borp/agent/sessions/92ab73d7-dd71-9347-f491-5b4260961214:detectIntent"
+
+    def call_model(self):
+        res = requests.post(
+            url=self.url,
+            data = json.dumps(self.data),
+            headers=self.headers
+        )
+        return res.text
+
+    def parse_output(self):
+        res = json.loads(self.call_model())
+        print(res)
+
+    def exec(self):
+        self.parse_output()
+
+IR = IntentRecognizer("where can I get a burger?")
+IR.exec()
